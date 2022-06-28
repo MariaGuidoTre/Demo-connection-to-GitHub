a348e6a8584d5110a803fc9a5081a917299c4346
Maria <guidotrejomaria@gmail.com>
2022-06-28 11:16:22 -0500

    Initial Commit

---
+++ Demo connection to GitHubTests/Demo_connection_to_GitHubTests.swift
@@ -0,0 +1,36 @@
+//
+//  Demo_connection_to_GitHubTests.swift
+//  Demo connection to GitHubTests
+//
+//  Created by Joshua  on 6/28/22.
+//
+
+import XCTest
+@testable import Demo_connection_to_GitHub
+
+class Demo_connection_to_GitHubTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
